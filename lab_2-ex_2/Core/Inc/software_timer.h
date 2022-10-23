6f6cc4622814edb761a4c69943bd7a421c9155a2
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2022-10-23 23:38:54 +0700

    lab2

---
+++ lab_2-ex_2/Core/Inc/software_timer.h
@@ -0,0 +1,15 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 5, 2022
+ *      Author: ADMIN
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int time1_flag;
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
---
+++ lab_2-ex_2/Core/Src/software_timer.c
@@ -0,0 +1,12 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Oct 5, 2022
+ *      Author: ADMIN
+ */
+#include "software_timer.h"
+
+int timer1_counter = 0;
+int timer1_flag = 0;
+
+
