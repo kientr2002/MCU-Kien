139d426b39af152496a474f537d8453767ede1a4
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2022-11-22 16:15:05 +0700

    add led_red to test activity

--- Code/lab4/Debug/Core/Src/sch.d
+++ Code/lab4/Debug/Core/Src/sch.d
@@ -22,7 +22,8 @@ Core/Src/sch.o: ../Core/Src/sch.c ../Core/Inc/sch.h ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
- ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ ../Core/Inc/timer.h
 
 ../Core/Inc/sch.h:
 
@@ -75,3 +76,5 @@ Core/Src/sch.o: ../Core/Src/sch.c ../Core/Inc/sch.h ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+
+../Core/Inc/timer.h:
